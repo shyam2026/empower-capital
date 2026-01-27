9ac77cabf180e287120a96da00b9a3d71151d53d
Shyam <shyam.ssc2015@gmail.com>
2026-01-27 19:20:21 +0530

    Fix Next.js config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-}
+// /** @type {import('next').NextConfig} */
+//const nextConfig = {
+ // images: {
+  //  domains: ['images.unsplash.com'],
+ // },
+//}
 
 module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
